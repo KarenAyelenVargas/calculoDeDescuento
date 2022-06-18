933af5c3e0723b7676459692d0203eb2441a837e
KarenAyelenVargas <103613115+KarenAyelenVargas@users.noreply.github.com>
2022-06-18 16:40:18 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let precioInicial: number = 450.5;
+let descuento: number = 0.1;
+let precioFinal: number = precioInicial * descuento;
+
+console.log(
+  "El precio final, tras aplicar el 10% de descuento es:" + precioFinal
+);
